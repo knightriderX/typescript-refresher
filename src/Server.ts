c7a3127a63059eab9d813007f5d7418cf20c4b15
KnightriderX <kgurukuntala@gmail.com>
2022-03-03 22:52:20 +0530

    learning more on Generics, Promises, Modules, Strict checks, and other types

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,5 @@
-interface IServer {
+export interface IServer {
+
     startServer(): void
     stopServer(): void
 }
@@ -7,31 +8,29 @@ class Server implements IServer {
 
     public port: number;
     public address: string;
+    public date: string = '';
 
     constructor(port: number, address: string) {
         this.port = port;
         this.address = address;
+        //    this.date = ''
     }
 
-    startServer() {
-        console.log(`Starting server at : ${this.address}:${this.port}`)
+    async startServer() {
+        const data = await this.getData(123);
+        console.log(`Starting server at: ${this.address}: ${this.port}`)
+        return function () {
+            //    this.date = 5;
+        }
     }
 
     stopServer(): void { }
 
+    async getData(id: number): Promise<string> {
+        return 'some SPecial Data'
+    }
 
 }
 
-// class DbServer extends Server {
-//     constructor(port: number, address: string) {
-//         super(port, address)
-//         console.log('calling db server constructor')
-//     }
-// }
-
 const someServer: IServer = new Server(8080, 'localhost');
-
-someServer.startServer()
-const somePort = (someServer as any).port   // someServer.port is inaccessible.
-console.log(somePort);
-
+someServer.startServer();
